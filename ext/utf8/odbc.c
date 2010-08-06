7b12cb2647806f6523c6df11e40aaa99db411ced
Luke Chadwick <luke.a.chadwick@gmail.com>
2010-08-06 16:00:00 +1000

    Updated import of ruby-odbc to work better with Snow Leopard

---
+++ ext/utf8/odbc.c
@@ -0,0 +1,15 @@
+/*
+ * ODBC-Ruby binding
+ * Copyright (c) 2001-2007 Christian Werner <chw@ch-werner.de>
+ *
+ * See the file "COPYING" for information on usage
+ * and redistribution of this file and for a
+ * DISCLAIMER OF ALL WARRANTIES.
+ *
+ * $Id: odbc.c,v 1.3 2007/04/07 09:39:08 chw Exp chw $
+ */
+
+#define UNICODE
+#define _UNICODE
+
+#include "../odbc.c"
